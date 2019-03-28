ddea281ba081ea3b596f71c67d4eb8908cff5999
Lukanin Mikhail <mlukanin@ics.perm.ru>
2019-03-28 14:55:54 +0500

    Signed-off-by: Lukanin Mikhail <mlukanin@ics.perm.ru>

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@
 setInterval(function(){
 $('#timer').html(Number($('#timer').text()) + 1);
 }, 1000);
+
 // 6 секунда самолета
 setTimeout(function(){
 $('.human-air-flat').show();
 }, 6000)
+
 // 10 секунда самолета
 setTimeout(function(){
   $('.airplane').hide();
   $('.human-air-flat').hide();
 }, 10500);
+
+// 11 секунда самолета
+setTimeout(function(){
+  $('.cloud').addClass('clouds');
+}, 11500);
+
+// 13 секунда самолета
+setTimeout(function(){
+  $('.cloud').hide();
+  $('#white-line .line').show();
+  $('#parashutist1 .parashutist').show();
+}, 12500);
